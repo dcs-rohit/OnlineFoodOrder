16c0b78537865911898f6114d25f629f3b5383c9
Rohit <rohitchaurasiya.dcs19@vnsgu.ac.in>
2021-02-20 12:45:39 +0530

    Initial Commit

---
+++ OnlineFoodOrder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnlineFoodOrder
+//
+//  Created by differenz157 on 20/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
